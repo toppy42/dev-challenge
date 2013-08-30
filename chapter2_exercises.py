a225474d9cf8b9865c8d3c9e70bb9e17a8784671
toppy42 <toppy42@gmail.com>
2013-08-29 21:04:00 -0400

    10 day challenge

--- chapter2_exercises.py
+++ chapter2_exercises.py
@@ -1,3 +1,38 @@
 # Exercises for chapter 2:
 
-# Name:
+# Name:Ethan Puzarne
+
+# 2.1
+# the leading zero designates the number as an octal.  When python prints out, it prints as base 10
+
+
+# 2.2
+
+# the shell output 5, nothing, 6
+# the same in the script output nothing
+#5
+#x = 5
+#x + 1
+
+# the print statement required quotes on the var assign and the x threw a unassigned error
+print 5
+print 'x = 5'
+print x + 1
+
+
+
+# 2.3
+# 1. width/2 = 8
+# 2. widht/2.0 = 8.5
+# 3. height/3 = 4.0
+# 4. 1 + 2 * 5 = 11
+# 5. delimiter * 5 = '.....'
+
+
+
+
+
+# 2.4
+# 1. volume of radius = 5, 523.6
+# 2. $945.45
+# 3. 6:30:06
--- chapter3_exercises.py
+++ chapter3_exercises.py
@@ -1,3 +1,70 @@
 # Exercises for chapter 3:
 
-# Name:
+# Name: Ethan Puzarne
+
+
+def repeat_lyrics():
+	print_lyrics()
+	print_lyrics()
+
+def print_lyrics():
+	print "I'm a lumberjack, and I'm okay."
+	print "I sleep all night and I work all day."
+
+repeat_lyrics()
+# 3.1:  # "NameError: name 'repeat_lyrics' is not defined. "
+
+
+# 3.2
+# the program executes correctly because the call to print_lyrics 
+# is not actually executed until the call to repeat_lyrics which post both definitions
+
+
+
+# 3.3
+
+def right_justify (s):
+	align_column = 70
+	given_length = len(s)
+	space_require = align_column - given_length
+	print " " * space_require + s
+
+right_justify("Hello")
+right_justify("t")
+right_justify("This is a test sentence")
+
+
+
+
+# 3.4-1
+#def do_twice(f):
+#	f()
+#	f()
+
+#def print_spam():
+#	print 'spam'
+
+#do_twice(print_spam)
+
+# 3.4-2  
+def do_twice(f, val):
+	f(val)
+	f(val)
+
+# 3.4-3
+def print_twice(s):
+	print s
+	print s
+
+# 3.4-4
+do_twice(print_twice, "spam")
+
+# 3.4-5
+def do_four(f, val):
+	do_twice(f, val)
+	do_twice(f, val)
+
+do_four(print_twice, "spam")
+
+
+
